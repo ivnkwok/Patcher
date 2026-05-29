88fd933fd5a40153ca9f3e8ffb624580ddc9ab3b
Ivan Kwok <ivnkwok@gmail.com>
2026-05-29 12:25:39 -0700

    Update SoundHandler.java

--- src/main/java/club/sk1er/patcher/util/world/sound/SoundHandler.java
+++ src/main/java/club/sk1er/patcher/util/world/sound/SoundHandler.java
@@ -29,85 +29,36 @@ import java.util.Map.Entry;
 public class SoundHandler implements IResourceManagerReloadListener {
 
     public SoundHandler() {
-        this.previousActive = Display.isActive();
-        handleFocusChange();
+
     }
 
-    private final Map<ResourceLocation, PropertyData> data = new HashMap<>();
+    // private final Map<ResourceLocation, PropertyData> data = new HashMap<>();
 
     @SubscribeEvent
     public void onSound(PlaySoundEvent event) {
-        //#if MC==10809
-        ISound soundResult = event.result;
-        //#else
-        //$$ ISound soundResult = event.getResultSound();
-        //#endif
-        if (soundResult instanceof PositionedSoundAccessor) {
-            PositionedSoundAccessor result = (PositionedSoundAccessor) soundResult;
-
-            result.setVolume(result.getVolumeField() * getVolumeMultiplier(soundResult.getSoundLocation()));
-        }
+        //bob
     }
-
-    private boolean previousActive;
-    private float previousVolume = -1f;
 
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event) {
-        if (event.phase != TickEvent.Phase.START) return;
-        boolean active = Display.isActive();
-        if (active != previousActive) {
-            previousActive = active;
-            handleFocusChange();
-        }
+
     }
 
     private void handleFocusChange() {
-        if (!previousActive) {
-            SoundManager soundManager = ((SoundHandlerAccessor) Minecraft.getMinecraft().getSoundHandler()).getSndManager();
-            previousVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.MASTER);
-            if (previousVolume == -1f) return;
-            soundManager.setSoundCategoryVolume(SoundCategory.MASTER, PatcherConfig.unfocusedSounds * previousVolume);
-        } else {
-            ((SoundHandlerAccessor) Minecraft.getMinecraft().getSoundHandler()).getSndManager().setSoundCategoryVolume(SoundCategory.MASTER, previousVolume);
-            previousVolume = -1f;
-        }
     }
 
-    private float getVolumeMultiplier(ResourceLocation sound) {
-        PropertyData propertyData = data.get(sound);
-        if (propertyData != null) {
-            Object asAny = propertyData.getAsAny();
-            if (asAny instanceof Integer) return ((Integer) asAny).floatValue() / 100F;
-        }
-        return 1.0f;
-    }
+    // private float getVolumeMultiplier(ResourceLocation sound) {
+    //     PropertyData propertyData = data.get(sound);
+    //     if (propertyData != null) {
+    //         Object asAny = propertyData.getAsAny();
+    //         if (asAny instanceof Integer) return ((Integer) asAny).floatValue() / 100F;
+    //     }
+    //     return 1.0f;
+    // }
 
     @Override
     public void onResourceManagerReload(IResourceManager resourceManager) {
-        Map<ResourceLocation, SoundEventAccessorComposite> soundRegistry = ((SoundRegistryAccessor) ((SoundHandlerAccessor) Minecraft.getMinecraft().getSoundHandler()).getSndRegistry()).getSoundRegistry();
-        for (Entry<ResourceLocation, SoundEventAccessorComposite> entry : soundRegistry.entrySet()) {
-            data.computeIfAbsent(entry.getKey(), location -> {
-                    String name = getName(location);
-                    //#if MC==10809
-                    return ConfigUtil.createAndRegisterConfig(PropertyType.SLIDER,
-                        WordUtils.capitalizeFully(entry.getValue().getSoundCategory().getCategoryName()),
-                        "Sounds", name, "Sound Multiplier for " + name,
-                        100, 0, 200, __ -> {
-                        }
-                    );
-                    //#else
-                    //$$ String[] category = entry.getValue().getLocation().getResourcePath().split("\\.");
-                    //$$ return ConfigUtil.createAndRegisterConfig(PropertyType.SLIDER,
-                    //$$    WordUtils.capitalizeFully(category[0].replace("_", " ")),
-                    //$$    category.length > 2 ? WordUtils.capitalizeFully(category[1].replace("_", " ")) : "Sounds", name, "Sound Multiplier for " + name,
-                    //$$    100, 0, 200, __ -> {
-                    //$$    }
-                    //$$ );
-                    //#endif
-                }
-            );
-        }
+        //bob
     }
 
     private String getName(ResourceLocation location) {
